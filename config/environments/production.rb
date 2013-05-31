098b7d7be391a5e9eb6721c4983ed2ea30f00c64
Aurélien Vecchiato <aurelien.vecchiato@belighted.com>
2013-05-31 08:00:05 +0200

    Specify additional assets for precompilation.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Wws::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += %w(backbone/wws)
 end
